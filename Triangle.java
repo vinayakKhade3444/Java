16996f7d3580195659dc1caaebec6e7652652560
dsoxo1 <67099367+vinayakKhade3444@users.noreply.github.com>
2023-01-23 21:13:10 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+class Triangle
+{
+         public static void main(String args[])
+         {
+                   int b=10,h=15;
+                   float area;
+                   area=(b*h)/2;
+                   System.out.println("Area of triangle is "+area);
+         }
+}
